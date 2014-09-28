d2806ade60d45ffe52c26a4015496ab5541ddbc2
Joshua Sorrell <jsorr80@gmail.com>
2014-09-28 13:22:41 -0500

    Added grouped bar charts

--- js/bar_quad.js
+++ js/bar_quad.js
@@ -1,17 +1,44 @@
 var margin = {top: 50, right: 50, bottom: 50, left: 50},
-	width = 550 - margin.left - margin.right,
-	height = 290 - margin.top - margin.bottom;
+	width = 500 - margin.left - margin.right,
+	height = 400 - margin.top - margin.bottom;
 
 var key = function(d) {
 	return d.name;
 }
 
 var x = d3.scale.ordinal()
-	.rangeRoundBands([0, width], 0.05);
+	.rangeRoundBands([0, width], 0.2);	
 	
-var color = d3.scale.category20b();
+var x0 = d3.scale.ordinal()
+	.rangeRoundBands([0, width], 0.1);
+
+var x1 = d3.scale.ordinal();
+
+var y = d3.scale.linear()
+	.range([height, 0]);
 	
-var attribute_list = ['msrp', 'engine_size', 'horsepower', 'cmpg'];//['cmpg', 'hmpg']];	
+var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient('bottom')	
+		
+var grouped_xAxis = d3.svg.axis()
+		.scale(x0)
+		.orient('bottom')	
+	
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")	
+
+var color = d3.scale.ordinal()
+		.range(['#006ba4', '#ff800e', '#ababab', '#595959', '#5f9ed1']);
+	
+var mpg_svg = d3.select('.mpg').append('svg')
+		.attr('width', width + margin.left + margin.right + 300)
+		.attr('height', height + margin.top + margin.bottom)
+		.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+	
+var attribute_list = ['msrp', 'engine_size', 'horsepower'];//, 'mpg'];
 var svg = {};
 attribute_list.forEach(function(entry) {
 	svg[entry] = d3.select('.' + entry).append('svg')
@@ -22,20 +49,16 @@ attribute_list.forEach(function(entry) {
 });
 	
 d3.json('data/cars.json', function(error, data) {
-console.log(data.filter(function(d) {return +d.sports === 1; }));
 	data = data.filter(function(d) {return +d.sports === 1; });
 	data = data.slice(6, 11);
 
 	x.domain(data.map(key));
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient('bottom')
 	
 	attribute_list.forEach(function(entry) {
-		var g = svg[entry].append('g')
-			.attr('transform', 'translate(0,5)')
+
+		var g = svg[entry]
 			.append('g')
-			.attr('class', 'x-axis axis')
+			.attr('class', 'x axis')
 			.attr('transform', 'translate(0,' + height + ')')
 			.call(xAxis);
 			
@@ -48,11 +71,10 @@ console.log(data.filter(function(d) {return +d.sports === 1; }));
 
 		var yAxis = d3.svg.axis()
 			.scale(y)
-			.orient('left')
-			.ticks(5);
+			.orient('left');
 		
 		svg[entry].append('g')
-			.attr('class', 'y-axis axis')
+			.attr('class', 'y axis')
 			.call(yAxis);
 			
 		var bars = svg[entry].selectAll('rect')
@@ -66,7 +88,72 @@ console.log(data.filter(function(d) {return +d.sports === 1; }));
 			.attr('x', function(d, i) { return x(d.name); })
 			.attr('y', function(d) { return y(+d[entry]); })
 			.attr("fill", function(d){ return color(key(d)); });	
-	});	
+	});
+	
+	build_grouped_bars();
+	
+	
+	
+function build_grouped_bars() {
+	
+	
+	var keys = ['City MPG', 'Hwy MPG'];
+	var mpg = [{name: 'cmpg'}, {name: 'hmpg'}];
+		
+	mpg.forEach( function(d) {
+		d.cars = data.map(function(car) { return {name: car.name, value: +car[d.name]}; }); 
+	});
+		
+		x0.domain(keys);//mpg.map(function(d) { return d.name; }));
+		x1.domain(data.map(key)).rangeRoundBands([0, x0.rangeBand()], 0.2);
+				
+		y.domain([0, d3.max(mpg, function(d) { return d3.max(d.cars, function(d) { return d.value; }); })]);
+		
+		mpg_svg.append("g")
+				.attr("class", "x axis")
+				.attr("transform", "translate(0," + height + ")")
+				.call(grouped_xAxis);
+
+		mpg_svg.append("g")
+				.attr("class", "y axis")
+				.call(yAxis);
+				
+		var bars = mpg_svg.selectAll('.bar')
+				.data(mpg)
+			.enter().append('g')
+				.attr('class', 'g')
+				.attr('transform', function(d) { return 'translate(' + x0(d.name) + ',0)'; });
+				
+		bars.selectAll('rect')
+				.data(function(d) { return d.cars; })
+			.enter().append('rect')
+				.attr('width', x1.rangeBand())
+				.attr('x', function(d) { return x1(d.name); })
+				.attr('y', function(d) { return y(d.value); })
+				.attr('height', function(d) { return height - y(d.value); })
+				.style('fill', function(d) { return color(d.name); });
+				
+		var legend = mpg_svg.selectAll('.legend')
+				.data(data.map(key))
+			.enter().append('g')
+				.attr('class', 'legend')
+				.attr('transform', function(d, i) { return 'translate(200,' + i*20 + ')'; });
+				
+		legend.append('rect')
+			.attr('x', width - 18)
+			.attr('width', 18)
+			.attr('height', 18)
+			.style('fill', color);
+			
+		legend.append('text')
+			.attr('x', width - 24)
+			.attr('y', 9)
+			.attr('dy', '.35em')
+			.style('text-anchor', 'end')
+			.text(function(d) { return d; });
+			
+}
+
 });
 
 function wrap(text, width) {
